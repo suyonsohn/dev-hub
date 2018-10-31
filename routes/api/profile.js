797bb7dc457b5a166496b6aa7d1162a9408e417e
sys <suyonsohn@gmail.com>
2018-10-31 13:34:17 -0700

    Add GET api/profile/all route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,6 +32,28 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(404).json(err))
 })
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {}
+
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if (!profiles) {
+                errors.noProfiles = 'There are no profiles.'
+                res.status(404).json(errors)
+            }
+
+            res.json(profiles)
+        })
+        .catch(err => {
+            errors.noProfiles = 'No profiles are found.'
+            res.status(404).json(errors)
+        })
+})
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
